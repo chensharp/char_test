55f60a41663c7680434a9f948c800a22815df7dc
test1 <chensharps@163.com>
2017-08-31 19:39:39 +0800

    add

--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
-
+#include<string>
 using namespace std;
+
+
+
+
+/*
+从s中找出第一个T的所在下标
+*/
+int getindex(string s,string t){
+    int i=0,j=0;
+    while(i< s.size() && j< t.size()){
+        if(s[i] == t[j]){
+            i++;
+            j++;
+        }else{
+            i=i-j+1;
+            j=0;
+        }
+    }
+    if(j>=t.size()){
+        return 0;
+    }
+    else{
+        return i-t.size();
+    }
+}
+
+
 
 int main()
 {
     cout << "Hello world char test !" << endl;
+
+    string s1="abcdefgiiifg";
+    string s2 = "fg";
+    cout<<"s1="<<s1<<" s2="<<s2<<endl;
+    int a=getindex(s1,s2);
+    cout<<"index= "<<a<<endl;
+
+
+
     return 0;
 }
